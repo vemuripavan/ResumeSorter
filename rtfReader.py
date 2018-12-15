ba8f3b35f83e24d9ed9cb09266765ccf5cd1b7cc
Pavan Kumar Vemuri <pavankumar.vemuri@evry.com>
2018-12-16 01:13:15 +0530

    Add logic to convert RTF to Text

--- rtfReader.py
+++ rtfReader.py
@@ -1,9 +1,10 @@
 from ResumeReader import ResumeReader
-
-
+from striprtf.striprtf import rtf_to_text
+from pathlib import Path
 
 class rtfReader(ResumeReader):
-    #Method which will parse the docx file and return text
+    #Method which will parse the rtf file and return text
     def readResume(self,file):
-        
-        return text
+        rtf_path = Path(file)
+        with rtf_path.open() as source:
+            return rtf_to_text(source.read()) 
